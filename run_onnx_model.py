c0fdbfa8e612656f01eb12ad862f331b6e52cfe7
sergiorozada12 <sergiorozada12@gmail.com>
2022-03-15 13:39:14 +0100

    feature: onnx translation model

--- run_onnx_model.py
+++ run_onnx_model.py
@@ -4,7 +4,7 @@ from transformers import MarianMTModel, MarianTokenizer
 
 
 tokenizer = AutoTokenizer.from_pretrained('Helsinki-NLP/opus-mt-es-ca')
-session = InferenceSession("onnx/model.onnx")
+session = InferenceSession("onnx/test.onnx")
 # ONNX Runtime expects NumPy arrays as input
 inputs = tokenizer("Hola qué tal soy científico de datos", return_tensors="np")
 outputs = session.run(output_names=["last_hidden_state"], input_feed=dict(inputs))
--- src/models.py
+++ src/models.py
@@ -1,5 +1,7 @@
 import re
 from transformers import MarianMTModel, MarianTokenizer
+from onnxruntime import InferenceSession
+
 
 class TranslationModel():
     def __init__(self, name, split_regex):
@@ -21,3 +23,21 @@ class TranslationModel():
     def save_model(self, name):
         self.tokenizer.save_pretrained(name)
         self.model.save_pretrained(name)
+
+
+class TranslationModelOnnx():
+    def __init__(self, name_tokenizer, name_model, split_regex):
+        self.split_regex = split_regex
+        self.tokenizer = MarianTokenizer.from_pretrained(name_tokenizer)
+        self.session = InferenceSession(name_model)
+
+    def _prepare_text(self, text):
+        text_filtered = text.replace('\n', ' ').strip()
+        return re.split(self.split_regex, text_filtered)
+
+    def translate(self, text):
+        sentences = self._prepare_text(text)
+        batches = self.tokenizer.prepare_seq2seq_batch(sentences, return_tensors="pt")
+        batches_translated = self.session.run(output_names=["last_hidden_state"], input_feed=dict(batches))
+        sentences_translated = [self.tokenizer.decode(s, skip_special_tokens=True) for s in batches_translated]
+        return " ".join(sentences_translated)
