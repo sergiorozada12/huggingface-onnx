4a71b61d6e98ad89ac984c9c2c3cde65ab5c805d
sergiorozada12 <sergiorozada12@gmail.com>
2022-04-05 18:24:20 +0200

    feature: encoder and decoder models

--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
 import re
+import numpy as np
+import torch
 from transformers import MarianMTModel, MarianTokenizer
 from onnxruntime import InferenceSession
 
@@ -25,6 +27,52 @@ class TranslationModel():
         self.model.save_pretrained(name)
 
 
+class TranslationEncoder(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.encoder = model.model.encoder
+
+    def forward(self, input_ids, attention_mask):
+        return self.encoder(input_ids, attention_mask=attention_mask, return_dict=False)
+
+
+class TranslationDecoder(torch.nn.Module):
+    def __init__(self, model, max_length):
+        super().__init__()
+        self.lm_weights = model.model.shared.weight.clone().detach()
+        self.lm_bias = model.final_logits_bias.clone().detach()
+        self.decoder = model.model.decoder
+        self.max_length = max_length
+
+    def forward(self, input_ids, attention_mask, encoder_outputs, index):
+        mask = np.triu(np.ones((self.max_length, self.max_length)), 1)
+        mask[mask == 1] = -np.inf
+        causal_mask = torch.tensor(mask, dtype=torch.float)
+
+        hidden, = self.decoder(
+            input_ids=input_ids,
+            encoder_hidden_states=encoder_outputs,
+            encoder_padding_mask=attention_mask,
+            decoder_padding_mask=None,
+            decoder_causal_mask=causal_mask,
+            return_dict=False,
+            use_cache=False,
+        )
+
+        _, n_length, _ = hidden.shape
+        mask_selection = torch.arange(n_length, dtype=torch.float32) == index
+        mask_selection = mask_selection.view(1, -1, 1)
+        masked_selection = torch.multiply(hidden, mask)
+        summed = torch.sum(masked_selection, 1)
+        hidden_masked = torch.unsqueeze(summed, 1)
+
+        return torch.nn.functional.linear(
+            input=hidden_masked,
+            weight=self.lm_weights,
+            bias=self.lm_bias
+        )
+
+
 class TranslationModelOnnx():
     def __init__(self, name_tokenizer, name_model, split_regex):
         self.split_regex = split_regex
@@ -38,6 +86,7 @@ class TranslationModelOnnx():
     def translate(self, text):
         sentences = self._prepare_text(text)
         batches = self.tokenizer.prepare_seq2seq_batch(sentences, return_tensors="pt")
+        batches['']
         batches_translated = self.session.run(output_names=["last_hidden_state"], input_feed=dict(batches))
         sentences_translated = [self.tokenizer.decode(s, skip_special_tokens=True) for s in batches_translated]
         return " ".join(sentences_translated)
